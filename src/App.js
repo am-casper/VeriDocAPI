26e69972e8d6df49b693a88ee76898ae916f6f80
Aayushmaan <goforaayush@gmail.com>
2023-10-02 14:00:49 +0530

    feat: adds retrieve blockchain status

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import CryptoJS from "crypto-js";
 function App() {
   const [qr, setQr] = useState(null);
   const [fileurl, setFileurl] = useState("");
+  const [status, setStatus] = useState("");
+  const [details, setDetails] = useState(null);
   function generateQR() {
     axios
       .post(
@@ -52,8 +54,40 @@ function App() {
       )
       .then((res) => {
         console.log(res.data);
+        getStatus();
       });
   };
+  const getStatus = () => {
+    const payloadNew = CryptoJS.SHA256(
+      process.env.REACT_APP_APP_KEY +
+        qr?.uniqueId +
+        process.env.REACT_APP_CLIENT_KEY
+    ).toString();
+    axios
+      .post(
+        "https://veridocglobal.com/api/getblockchainstatus",
+        {
+          uniqueId: qr?.uniqueId,
+        },
+        {
+          headers: {
+            apikey: process.env.REACT_APP_APP_KEY,
+            payload: payloadNew,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        console.log(payloadNew);
+        if (res.data.blockchainstatus !== "Success") {
+          getStatus();
+        } else{
+          setDetails(res.data);
+        }
+        setStatus(res.data.blockchainstatus);
+        
+      });
+  }
   return (
     <div>
       <button onClick={generateQR}>Generate QR</button>
@@ -63,6 +97,15 @@ function App() {
       File Url:
       <input type="text" value={fileurl} onChange={handleInput} />
       <button onClick={handleSubmit}>Submit</button>
+      <p>Blockchain Status:{status===""?`Upload document`:status}</p>
+      {
+        details&&(
+          <div>
+            <p>Transaction ID:{details.transactionid}</p>
+            <p>Blockchain URL:<a href={details.blockchainurl}>{details.blockchainurl}</a></p>
+          </div>
+        )
+      }
     </div>
   );
 }
